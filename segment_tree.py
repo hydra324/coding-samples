c6f5c507ec05d33295f6dd90dbdc9e041294781f
hydra324 <akhilchilumuru@gmail.com>
2023-08-10 15:35:17 -0500

    added segment tree for min query

--- segment_tree.py
+++ segment_tree.py
@@ -39,3 +39,44 @@ def update(v,l,r,pos,val):
         update(2*v+1,mid+1,r,pos,val)
     tree[v] = tree[2*v]+tree[2*v+1]
     return
+
+# segment tree for min in range query
+arr = [1,3,-5,8,9]
+tree = [float('inf')]*len(arr)*4
+def build(v,l,r):
+    '''
+        v: current vertex
+        l,r: left and right boundary of current segments
+    '''
+    if l==r:
+        tree[v]=arr[l]
+    mid = (l+r)//2
+    return min(build(2*v,l,mid),build(2*v+1,mid+1,r))
+
+def find(v,l,r,ql,qr):
+    '''
+        finds minimm in the given range
+    '''
+    if ql>qr:
+        return float('inf')
+    if ql==l and qr==r:
+        return tree[v]
+    # check if belongs to left or right child
+    mid = (l+r)//2
+    return min(find(2*v,l,mid,ql,min(qr,mid)),find(2*v,mid+1,r,max(ql,mid+1),qr))
+
+def update(v,l,r,pos,val):
+    '''
+        updates the value at the given position and still retains the segment tree
+    '''
+    if l==r:
+        tree[v] = val
+        return
+    mid = (l+r)//2
+    if pos<=mid:
+        update(2*v,l,mid,pos,val)
+    else:
+        update(2*v+1,mid+1,r,pos,val)
+    tree[v] = min(tree[2*v],tree[2*v+1])
+
+    
