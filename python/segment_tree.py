26a9adc575eb4d747abf6281779f7906060b4bfb
hydra324 <akhilchilumuru@gmail.com>
2023-08-31 16:49:16 -0500

    fixed segment tree python version

--- python/segment_tree.py
+++ python/segment_tree.py
@@ -1,3 +1,6 @@
+# Always try to use 1-based index for segment tree.
+# root node at index 1 will have information over the entire array
+
 # segment tree for sum query
 arr = [1,3,-5,6,8,9]
 tree = [0]*len(arr)*4
@@ -7,7 +10,9 @@ def build(v,l,r):
         tree[v] = arr[l]
         return
     mid = (l+r)//2
-    return build(2*v,l,mid)+build(2*v+1,mid+1,r)
+    build(2*v,l,mid)
+    build(2*v+1,mid+1,r)
+    tree[v] = tree[2*v]+tree[2*v+1]
 
 def sum(v,l,r,ql,qr):
     '''
@@ -20,7 +25,12 @@ def sum(v,l,r,ql,qr):
     if ql==l and qr==r:
         return tree[v]
     mid = (l+r)//2
-    return sum(2*v,l,mid,ql,min(mid,qr))+sum(2*v+1,mid+1,r,max(mid+1,ql),qr)
+    if qr<=mid:
+        return sum(2*v,l,mid,ql,qr)
+    elif ql>mid:
+        return sum(2*v+1,mid+1,r,ql,qr)
+    else:
+        return sum(2*v,l,mid,ql,mid)+sum(2*v+1,mid+1,r,mid+1,qr)
 
 def update(v,l,r,pos,val):
     '''
@@ -50,8 +60,12 @@ def build(v,l,r):
     '''
     if l==r:
         tree[v]=arr[l]
+        return
     mid = (l+r)//2
-    return min(build(2*v,l,mid),build(2*v+1,mid+1,r))
+    build(2*v,l,mid)
+    build(2*v+1,mid+1,r)
+    tree[v]=min(tree[2*v],tree[2*v+1])
+    return
 
 def find(v,l,r,ql,qr):
     '''
@@ -63,7 +77,12 @@ def find(v,l,r,ql,qr):
         return tree[v]
     # check if belongs to left or right child
     mid = (l+r)//2
-    return min(find(2*v,l,mid,ql,min(qr,mid)),find(2*v,mid+1,r,max(ql,mid+1),qr))
+    if qr<=mid:
+        return find(2*v,l,mid,ql,qr)
+    elif ql>mid:
+        return find(2*v+1,mid+1,r,ql,qr)
+    else:
+        return min(find(2*v,l,mid,ql,mid),find(2*v+1,mid+1,r,mid+1,qr))
 
 def update(v,l,r,pos,val):
     '''
